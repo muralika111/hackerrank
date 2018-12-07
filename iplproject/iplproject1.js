dd9bc2389d5c83b921efa69dd44aa1a0cdf4ce0c
Murali <findmurali97@gmail.com>
2018-12-07 15:07:07 +0530

    project1 commit 1

---
+++ iplproject/iplproject1.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var matches = JSON.parse(fs.readFileSync('matches.json', 'utf-8'));
+obj={};
+for(let i in matches){
+    if (obj.hasOwnProperty(matches[i]["season"])){
+        obj[matches[i]["season"]]+=1;
+    }
+    else{
+        obj[matches[i]["season"]]=0;
+
+    }
+
+}
+console.log(obj)
